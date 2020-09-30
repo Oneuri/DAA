78a3c5b65cd12723fee7dda8d66c62d57fbe0161
uri <59758205+Oneuri@users.noreply.github.com>
2020-09-30 09:38:07 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,15 @@
+print("Enter value of a")
+a=int(input())
+print("Enter value of b")
+b=int(input())
+a = a+b
+b = a-b
+a = a-b
+
+
+print('New value of a is:')
+print(a)
+
+
+print('New value of b is:')
+print(b)
